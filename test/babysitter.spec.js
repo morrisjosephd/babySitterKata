7b0a7f6022376501cd0752b347f3e6c437e64540
Joe Morris <jmorris@pillartechnology.com>
2016-01-27 16:03:55 -0500

    refactor tests

--- test/babysitter.spec.js
+++ test/babysitter.spec.js
@@ -2,6 +2,59 @@ var chai = require('chai');
 var assert = chai.assert;
 var babysitter = require('./../babysitter.js');
 
+
+describe('A babysitter', () => {
+
+  it('should be paid $12 for one hour of work before midnight', () => {
+    var results  = babysitter(setTime(5), setTime(6), setTime(10));
+
+    assert.equal(results, 12);
+  });
+
+  it('should be paid $24 for two hours of work before midnight', () => {
+    var results  = babysitter(setTime(5), setTime(7), setTime(10));
+
+    assert.equal(24, results);
+  });
+
+  it('should be paid $20 for 1 hour before bedtime and 1 hour after bedtime', () => {
+    var results  = babysitter(setTime(5), setTime(7), setTime(6));
+
+    assert.equal(20, results);
+  });
+
+  it('should be paid $28 for 1 hour before bedtime and 2 hours after bedtime', () => {
+    var results  = babysitter(setTime(5), setTime(8), setTime(6));
+
+    assert.equal(28, results);
+  });
+
+  it('should be paid $16 for 1 hour after midnight', () => {
+    var results  = babysitter(setTime(12), setTime(1), setTime(1));
+
+    assert.equal(16, results);
+  });
+
+  it('should be paid $28 for 1 hour awake before midnight and 1 hour after midnight', () => {
+    var results  = babysitter(setTime(11), setTime(1), setTime(12));
+
+    assert.equal(28, results);
+  });
+
+  it('should be paid $24 for 1 hour in bed before midnight and 1 hour after midnight', () => {
+    var results  = babysitter(setTime(11), setTime(1), setTime(11));
+
+    assert.equal(24, results);
+  });
+
+  it('should not be paid for not working any hours', () => {
+    var results  = babysitter(setTime(7), setTime(7), setTime(8));
+
+    assert.equal(0, results);
+  })
+
+});
+
 function setTime(hour) {
   var d = new Date();
   d.setHours(hour);
@@ -9,79 +62,3 @@ function setTime(hour) {
   d.setSeconds(0);
   return d;
 }
-
-describe('A babysitter', function() {
-
-  it('should be paid $12 for one hour of work before midnight', function() {
-    var startTime = setTime(5);
-    var endTime =  setTime(6);
-    var bedTime = setTime(10);
-    var results  = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(results, 12);
-  });
-
-  it('should be paid $24 for two hours of work before midnight', function() {
-    var startTime = setTime(5);
-    var endTime =  setTime(7);
-    var bedTime = setTime(10);
-    var results  = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(24, results);
-  });
-
-  it('should be paid $20 for 1 hour before bedtime and 1 hour after bedtime', function() {
-    var startTime = setTime(5);
-    var endTime = setTime(7);
-    var bedTime = setTime(6);
-    var results = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(20, results);
-  });
-
-  it('should be paid $28 for 1 hour before bedtime and 2 hours after bedtime', function() {
-    var startTime = setTime(5);
-    var endTime = setTime(8);
-    var bedTime = setTime(6);
-    var results = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(28, results);
-  });
-
-  it('should be paid $16 for 1 hour after midnight', function() {
-    var startTime = setTime(12);
-    var endTime = setTime(1);
-    var bedTime = setTime(1);
-    var results = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(16, results);
-  });
-
-  it('should be paid $28 for 1 hour awake before midnight and 1 hour after midnight', function() {
-    var startTime = setTime(11);
-    var endTime = setTime(1);
-    var bedTime = setTime(12);
-    var results = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(28, results);
-  });
-
-  it('should be paid $24 for 1 hour in bed before midnight and 1 hour after midnight', function() {
-    var startTime = setTime(11);
-    var endTime = setTime(1);
-    var bedTime = setTime(11);
-    var results = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(24, results);
-  });
-
-  it('should not be paid for not working any hours', function() {
-    var startTime = setTime(7);
-    var endTime = setTime(7);
-    var bedTime = setTime(8);
-    var results = babysitter(startTime, endTime, bedTime);
-
-    assert.equal(0, results);
-  })
-
-});
